25c9b3eee365bfc36cd5adac4db49156cd6c29a0
Yavuz <yavuz.orbey@gmail.com>
2021-03-30 15:36:25 -0400

    commit

---
+++ src/testing.java
@@ -0,0 +1,9 @@
+public  class testing{
+    public static void main(String[] args) {
+        System.out.println("Hello testing");
+        System.out.println("");
+        System.out.println("dsdsds");
+        String s = "dsdasd";
+        System.out.println(s.indexOf('d'));
+    }
+}
